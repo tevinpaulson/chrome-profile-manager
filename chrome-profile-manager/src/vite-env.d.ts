5cdf93e30fb2aeff2760914d0bc8e89b53bd0882
renzabc <renzagamanos3@gmail.com>
2025-03-04 20:21:57 -0600

    declared global variabled so that main process functions are recognized on the renderer process

--- chrome-profile-manager/src/vite-env.d.ts
+++ chrome-profile-manager/src/vite-env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="vite/client" />
+declare global {
+    interface Window {
+      system: {
+        getBrowserPath, // Adjust the return type based on what getPath returns
+        saveProfiles,// Use later for new feature
+        readSettings // Use later for new feature
+      },
+      tasks:{
+        deleteProfile,
+        killBrowsers,
+        launchBrowser,
+      }
+      thiswindow:{
+        minimize,
+        maximize,
+        close
+      }
+    }
+  }
+  
+  // This is required to make the file a module
+  export {};
